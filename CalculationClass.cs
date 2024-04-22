ec9577be3703b67fa18a5cfe92c21a80cc03eec6
Anastasia <girl-physicist@mail.ru>
2024-04-22 10:51:35 +0300

    Add calc T[i]

--- CalculationClass.cs
+++ CalculationClass.cs
@@ -23,63 +23,88 @@ namespace SteamAndGasCloud
         {
 
         }
-
-        public void СalculateTemperature()
-        {
-
-        }
-
-
+        //ro1[i+1]
         public double СalculateDensityOfAblationProducts(double deltaFunc, double constValueA, double currentTime, double tDelta, double Vi, double ViPlus1, double ro1i)
         {
             return ro1i + (GetConstWithDeltaFunc(deltaFunc, constValueA, currentTime) - GetAcomponentFromDensity(currentTime, tDelta, Vi, ViPlus1, ro1i)) / GetValueDeltaRDeltaT(tDelta, Vi, currentTime - tDelta, currentTime);
         }
+        //ro2[i+1]
         public double СalculateDensityOfWaterVapor(double deltaFunc, double constValueB, double currentTime, double tDelta, double Ui, double UiPlus1, double ro2i)
         {
             return ro2i + (GetConstWithDeltaFunc(deltaFunc, constValueB, currentTime) - GetAcomponentFromDensity(currentTime, tDelta, Ui, UiPlus1, ro2i)) / GetValueDeltaRDeltaT(tDelta, Ui, currentTime - tDelta, currentTime);
 
         }
+        //V[i+1]
+        public double СalculateFlowRateOfAblationProducts(double currentTime, double tDelta, double Vi, double piPlus1, double pi, double ro1i)
+        {
+            return GetAcomponentFromFlowRate(currentTime, tDelta, Vi) * GetBcomponentFromFlowRate(currentTime, tDelta, Vi) - GetCcomponentFromFlowRate(currentTime, tDelta, Vi, piPlus1, pi, ro1i);
+        }
+        //U[i+1]
+        public double СalculateFlowRateOfWaterVapor(double currentTime, double tDelta, double Ui, double piPlus1, double pi, double ro2i)
+        {
+            return GetAcomponentFromFlowRate(currentTime, tDelta, Ui) * GetBcomponentFromFlowRate(currentTime, tDelta, Ui) - GetCcomponentFromFlowRate(currentTime, tDelta, Ui, piPlus1, pi, ro2i);
+        }
+        // P[i+1]
+        public double СalculatePressureOfSteamGasMixture(double deltaFunk, double constA, double constB, double currentTime, double tDelta, double Ui, double Vi, double ro1i, double ro2i, double pi, double Ti)
+        {
+            return GetAcomponentFromPi(currentTime, tDelta, Vi, Ui, Ti) * GetBcomponentFromPi(deltaFunk, constA, constB, currentTime, tDelta, Ui, Vi, ro1i, ro2i, pi);
+        }
+        // T[i]
+        public double СalculateTemperature(double ro1i, double ro2i, double tDelta, double Vi, double Ui, double currentTime, double TiMinus1, double TiPlus1)
+        {
+            return GetUpFromT(ro1i, ro2i, tDelta, Vi, Ui, currentTime, TiMinus1, TiPlus1) / GetDownFromT(ro1i, ro2i, tDelta, Vi, Ui, currentTime);
+        }
 
+        private double GetUpFromT(double ro1i, double ro2i, double tDelta, double Vi, double Ui, double currentTime, double TiMinus1, double TiPlus1)
+        {
+            var up1 = GetAcomponentFromT(ro1i, Vi, ClassConst.Cp1, currentTime, tDelta);
+            var up2 = GetAcomponentFromT(ro2i, Ui, ClassConst.Cp2, currentTime, tDelta);
+
+            var up3 = GetBcomponentFromT(currentTime, tDelta);
+            var up4 = 2 / GetRadius(currentTime) + 1 / GetDeltaRadius(currentTime - tDelta, currentTime);
+
+            var up5 = up3 * TiMinus1 / GetDeltaRadius(currentTime - tDelta, currentTime);
+            return TiPlus1 * (up1 + up2 - up3 * up4) - up5;
+        }
+        private double GetDownFromT(double ro1i, double ro2i, double tDelta, double Vi, double Ui, double currentTime)
+        {
+            var down1 = GetAcomponentFromT(ro1i, Vi, ClassConst.Cp1, currentTime, tDelta);
+            var down2 = GetAcomponentFromT(ro2i, Ui, ClassConst.Cp2, currentTime, tDelta);
+
+            var down3 = 2 * GetBcomponentFromT(currentTime, tDelta) * (1 / GetRadius(currentTime) + 1 / GetDeltaRadius(currentTime - tDelta, currentTime));
+
+            return down1 + down2 - down3;
+        }
+        private double GetAcomponentFromT(double roi, double value, double cp, double currentTime, double tDelta)
+        {
+            return roi * cp * GetValueDeltaRDeltaT(tDelta, value, currentTime - tDelta, currentTime);
+        }
+        private double GetBcomponentFromT(double currentTime, double tDelta)
+        {
+            return (ClassConst.alpha1 + ClassConst.alpha2) / GetDeltaRadius(currentTime - tDelta, currentTime);
+        }
         private double GetAcomponentFromDensity(double currentTime, double tDelta, double value, double valuePlus1, double ro)
         {
             var a1 = Get2ValueToRi(value, currentTime);
             var a2 = (valuePlus1 - value) / GetDeltaRadius(currentTime - tDelta, currentTime);
             return ro * (a1 + a2);
         }
-
-
-        public double СalculateFlowRateOfAblationProducts(double currentTime, double tDelta, double Vi, double piPlus1, double pi, double ro1i)
-        {
-            return GetAcomponentFromFlowRate(currentTime, tDelta, Vi) * GetBcomponentFromFlowRate(currentTime, tDelta, Vi) - GetCcomponentFromFlowRate(currentTime, tDelta, Vi, piPlus1, pi, ro1i);
-        }
-        public double СalculateFlowRateOfWaterVapor(double currentTime, double tDelta, double Ui, double piPlus1, double pi, double ro2i)
-        {
-            return GetAcomponentFromFlowRate(currentTime, tDelta, Ui) * GetBcomponentFromFlowRate(currentTime, tDelta, Ui) - GetCcomponentFromFlowRate(currentTime, tDelta, Ui, piPlus1, pi, ro2i);
-        }
-
         private double GetAcomponentFromFlowRate(double currentTime, double tDelta, double value)
         {
             return value / GetValueDeltaRDeltaT(tDelta, value, currentTime - tDelta, currentTime);
         }
-
         private double GetBcomponentFromFlowRate(double currentTime, double tDelta, double value)
         {
             var b1 = Get2ValueToRi(value, currentTime);
             var b2 = GetValueDeltaRDeltaT(tDelta, value, currentTime - tDelta, currentTime);
             return b2 - b1;
         }
-
         private double GetCcomponentFromFlowRate(double currentTime, double tDelta, double value, double piPlus1, double pi, double roi)
         {
             var c1 = piPlus1 - pi;
             var c2 = GetValueDeltaRDeltaT(tDelta, value, currentTime - tDelta, currentTime) * roi * GetDeltaRadius(currentTime - tDelta, currentTime);
             return c1 / c2;
         }
-        public double СalculatePressureOfSteamGasMixture(double deltaFunk, double constA, double constB, double currentTime, double tDelta, double Ui, double Vi, double ro1i, double ro2i, double pi)
-        {
-            return GetAcomponentFromPi(currentTime, tDelta, Vi, Ui) * GetBcomponentFromPi(deltaFunk, constA, constB, currentTime, tDelta, Ui, Vi, ro1i, ro2i, pi);
-        }
-
         private double GetConstWithDeltaFunc(double deltaFunc, double constValue, double currentTime)
         {
             return constValue * deltaFunc * (GetRadius(currentTime) - ClassConst.r0);
@@ -102,10 +127,9 @@ namespace SteamAndGasCloud
 
             return c1 + c3 + c5 - c7 + c2 + c4 + c6 - c8;
         }
-
-        private double GetAcomponentFromPi(double currentTime, double tDelta, double Vi, double Ui)
+        private double GetAcomponentFromPi(double currentTime, double tDelta, double Vi, double Ui, double Ti)
         {
-            var up = ClassConst.universalGasConstant * GetTi();
+            var up = ClassConst.universalGasConstant * Ti;
 
             var down1 = 1 - up;
             var deltaR = GetDeltaRadius(currentTime - tDelta, currentTime);
@@ -123,13 +147,6 @@ namespace SteamAndGasCloud
 
             return up / (down1 * (down2 + down3));
         }
-
-
-        private double GetTi()
-        {
-            return 0.0;
-        }
-
         private double GetCcomponentFromPi(double value, double currentTime, double pi, double roi, double tDelta, double mu)
         {
             var up = GetNumeratorCcomponentFromPi(value, currentTime, pi, roi, tDelta);
@@ -139,8 +156,6 @@ namespace SteamAndGasCloud
 
             return up / down2;
         }
-
-
         //value can be Vi or Ui
         //roi can be ro1i or ro2i
         private double GetNumeratorCcomponentFromPi(double value, double currentTime, double pi, double roi, double tDelta)
@@ -161,7 +176,6 @@ namespace SteamAndGasCloud
 
             return a + b;
         }
-
         private double GetDeltaRadius(double time1, double time2)
         {
             return GetRadius(time2) - GetRadius(time1);
